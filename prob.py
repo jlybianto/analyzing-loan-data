c0dfcf2b2d6010192aa5690bacd17e3fa827d588
jlybianto <jesse.lybianto@gmail.com>
2015-11-05 14:15:19 -0700

    Add data to by analyzed, matplotlib and box-plot

---
+++ prob.py
@@ -0,0 +1,14 @@
+# The matplotlib package is for graphical outputs (eg. box-plot, histogram, QQ-plot).
+import matplotlib.pyplot as plt
+
+# Data to be analyzed.
+x = [
+1, 1, 1, 1, 1, 1, 1, 1, 
+2, 2, 2, 3, 4, 4, 4, 4, 
+5, 6, 6, 6, 7, 7, 7, 7, 
+7, 7, 7, 7, 8, 8, 9, 9
+]
+
+# Generate and save a box-plot of the data.
+plt.boxplot(x)
+plt.savefig("boxplot.png")
