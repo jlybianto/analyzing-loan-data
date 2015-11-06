0cea7a5f0ecf00727cd04f455149e3868dd82936
jlybianto <jesse.lybianto@gmail.com>
2015-11-05 23:51:21 -0700

    Add scipy module and qqplot of data

--- prob_lending_club.py
+++ prob_lending_club.py
@@ -1,7 +1,9 @@
 # The matplotlib package is for graphical outputs (eg. box-plot, histogram, QQ-plot).
 # The pandas package is used to fetch and store data in a DataFrame.
+# The scipy.stats module is for a large number of probability distributions and statistical functions.
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy.stats as stats
 
 # Collection of data from Amazon AWS.
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
@@ -18,3 +20,8 @@ plt.savefig("boxplot-amountrequested.png")
 plt.figure()
 loansData.hist(column='Amount.Requested')
 plt.savefig("hist-amountrequested.png")
+
+# Generate and save a QQ-plot of the data.
+plt.figure()
+graph = stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
+plt.savefig("qqplot-amountrequested.png")
