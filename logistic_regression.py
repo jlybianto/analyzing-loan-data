63d8afdf4435aae4bd3b0ea8df8144a7858a0c56
jlybianto <jesse.lybianto@gmail.com>
2015-11-10 23:20:31 -0700

    Make a new column where interest rate results in a boolean

--- logistic_regression.py
+++ logistic_regression.py
@@ -7,9 +7,12 @@ import statsmodels.api as sm
 loansData = pd.read_csv("https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv")
 
 # Clean data from columns 'Interest.Rate', 'Loan.Length', 'FICO.Range' into numerical types.
-# Convert interest rate from 'xx.xx%' to '0.xxxx'
+# Convert interest rate from 'xx.xx%' to 'xx.xx'
 # Convert length from 'xx months' to 'xx'
 # Convert score from categorical 'xxx-yyy' to 'xxx'
-loansData['Interest.Rate'] = [round((float(i[:-1]) / 100), 4) for i in loansData['Interest.Rate']]
+loansData['Interest.Rate'] = [round(float(i[:-1]), 2) for i in loansData['Interest.Rate']]
 loansData['Loan.Length'] = [int(month.rstrip(" months")) for month in loansData['Loan.Length']]
 loansData['FICO.Range'] = [int((score.split("-"))[0]) for score in loansData['FICO.Range']]
+
+# Convert interest rate to boolean figure 0 when the rate is less than 12.00% and 1 otherwise.
+loansData['Int.Rate.Bool'] = [0 if i < 12.00 else 1 for i in loansData['Interest.Rate']]
