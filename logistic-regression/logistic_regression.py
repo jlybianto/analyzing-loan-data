58cdf1e75df5a46da35cbfe6fe0b967e04249722
jlybianto <jesse.lybianto@gmail.com>
2015-11-12 14:51:33 -0700

    Create a list of independent variables

--- logistic-regression/logistic_regression.py
+++ logistic-regression/logistic_regression.py
@@ -9,10 +9,10 @@ loansData = pd.read_csv("https://spark-public.s3.amazonaws.com/dataanalysis/loan
 # Clean data from columns 'Interest.Rate', 'Loan.Length', 'FICO.Range' into numerical types.
 # Convert interest rate from 'xx.xx%' to 'xx.xx'
 # Convert length from 'xx months' to 'xx'
-# Convert score from categorical 'xxx-yyy' to 'xxx'
+# Convert range to score from by picking the lower end 'xxx-yyy' to 'xxx'
 loansData['Interest.Rate'] = [round(float(i[:-1]), 2) for i in loansData['Interest.Rate']]
 loansData['Loan.Length'] = [int(month.rstrip(" months")) for month in loansData['Loan.Length']]
-loansData['FICO.Range'] = [int((score.split("-"))[0]) for score in loansData['FICO.Range']]
+loansData['FICO.Score'] = [int((score.split("-"))[0]) for score in loansData['FICO.Range']]
 
 # Convert interest rate to boolean (T/F) figures. (0) when the rate is less than 12.00% and (1) otherwise.
 loansData['Int.Rate.Bool'] = [0 if i < 12.00 else 1 for i in loansData['Interest.Rate']]
@@ -21,3 +21,4 @@ loansData['Int.Rate.Bool'] = [0 if i < 12.00 else 1 for i in loansData['Interest
 loansData['Intercept'] = 1
 
 # Create a list of column names of independent variables.
+ind_vars = ['FICO.Score', 'Amount.Requested', 'Intercept']
