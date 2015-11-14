9f27cfddeb4afff54d757005f1234bd82ec29231
jlybianto <jesse.lybianto@gmail.com>
2015-11-14 03:49:04 -0700

    Add packages to be used

---
+++ multivariate/multivariate.py
@@ -0,0 +1,8 @@
+# The pandas package is used to fetch and store data in a DataFrame.
+# The numpy package is for scientific computing and container of generic data (used for generating a continuous distribution)
+# The statsmodels is used to find the model coefficients.
+# The matplotlib package is for graphical outputs (eg. box-plot, histogram, QQ-plot).
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
