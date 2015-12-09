ce42710e5f4c5f36243b3f6fff640abb9db5f1f5
jlybianto <jesse.lybianto@gmail.com>
2015-12-08 19:21:59 -0700

    Add comments for development

--- markov-model/markov.py
+++ markov-model/markov.py
@@ -4,9 +4,12 @@ import pandas as pd
 # Model probabilities of transitioning financial markets.
 df = pd.DataFrame(
 	{
-	"Bear": [0.8, 0.15, 0.05],
-	"Bull": [0.075, 0.9, 0.025],	
-	"Stagnant": [0.25, 0.25, 0.5]
+	"Bear": [0.8, 0.075, 0.25],
+	"Bull": [0.15, 0.9, 0.25],	
+	"Stagnant": [0.05, 0.025, 0.5]
 	}, index=["Bear", "Bull", "Stagnant"])
 
 print df
+
+# Need to convert DataFrame to an array or matrix
+# Create a function that will take user input of desired iteration of transition
