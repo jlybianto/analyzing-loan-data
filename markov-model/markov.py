0e136ef2cd2c6fa0df31efd128dbcbb9d53a3ddf
jlybianto <jesse.lybianto@gmail.com>
2015-12-09 22:17:37 -0700

    Add statements and whitespace for clarity

--- markov-model/markov.py
+++ markov-model/markov.py
@@ -11,15 +11,23 @@ df = pd.DataFrame(
 	"Stagnant": [0.05, 0.025, 0.5]
 	}, index=["Bear", "Bull", "Stagnant"])
 
+print ""
+print "Initial state of financial markets Markov model:"
+print ""
 print df
+print ""
 
 # Convert DataFrame to an array or matrix for computations.
 m = np.mat(df)
 
 # Create a function that will take user input of desired iteration of transition.
-count = raw_input("Insert number of transitions: ")
+count = int(raw_input("Insert number of transitions: "))
 if count == 0:
 	print "No transitions"
 else:
 	m = m ** (int(count) + 1)
+	m = pd.DataFrame(m, index=["Bear", "Bull", "Stagnant"], columns=["Bear", "Bull", "Stagnant"])
+	print ""
+	print "State of financial markets Markov model after " + str(count) + " transitions:"
+	print ""
 	print m
